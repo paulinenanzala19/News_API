e46951ddd8d54a62748f3c4ee4995f3f69bc87ef
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-04-30 20:11:09 +0300

    adding different classes of news

--- app/views.py
+++ app/views.py
@@ -7,4 +7,7 @@ from .request import get_news
 @News.route('/')
 def index():
     health_news=get_news('health')
-    return render_template('index.html', health=health_news)
+    sport_news=get_news('sports')
+    breaking_news=get_news('breaking')
+    entertainment_news=get_news('entertainment')
+    return render_template('index.html', health=health_news,sports=sport_news,breaking=breaking_news,entertainment=entertainment_news)
