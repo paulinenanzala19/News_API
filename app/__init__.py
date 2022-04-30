e7ad738b0ee0583b996a8886e1ab994b61906d92
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-01 00:48:44 +0300

    creating files under the template folder to separate concerns

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 News = Flask(__name__, instance_relative_config=True)
 News.config.from_object(DevConfig)
 News.config.from_pyfile('config.py')
+
+bootstrap= Bootstrap(News)
 
 from app import views
