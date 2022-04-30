4c8b62f96d50dc91665cb22a449cd3051c53df78
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-04-30 15:55:25 +0300

    creating class articles to define article objects

---
+++ app/models/news.py
@@ -0,0 +1,14 @@
+class Article:
+    """
+    Article class to define article objects
+    """
+
+    def __init__(self, id,title,description,time,url,author):
+        self.id=id
+        self.title=title
+        self.description=description
+        self.time=time
+        self.url=url
+        self.author=author
+        pass
+    
