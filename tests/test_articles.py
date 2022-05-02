ea1b33458e3f3b424165e278f3059a94ea2605dc
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-02 15:28:48 +0300

    adding the folder structure

--- tests/test_articles.py
+++ tests/test_articles.py
@@ -18,14 +18,6 @@ class TestArticle(unittest.TestCase):
  def test_instance():
     self.assertTrue(isinstance(self.news_object,Article))
  
-#  def test_init(self):
-#     self.assertEqual(self.new_object.id,'null')
-#     self.assertEqual(self.new_object.title,'Josef Fares doesnt think games need to be fun to suceed')
-#     self.assertEqual(self.new_object.description,'Josef Fares sat down with Rami Ismail for a GamesBeat Summit fireside chat to talk about passion drive and making games')
-#     self.assertEqual(self.new_object.url,'https://venturebeat.com/2022/4/29/fares-doesnt-think-games-need-to-be-fun')
-#     self.assertEqual(self.new_object.author,'Erron Kelly')
-#     self.assertEqual(self.new_.object.urlToImage,'https://venturebeat.com/wp-content/uploads/2022/04/faresTGA.jpg?w=1200&strip=all')
-#     self.assertEqual(self.new_object.publishedAt,'2022-04-29T17:30:00Z')
 
 if __name__ == "__main__":
     unittest.main()
